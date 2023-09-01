abdfe966f65815711d85537a25293f93c9836d99
Roberto Barreto <robertofmbarreto@gmail.com>
2023-09-01 14:48:33 +0100

    alerts page optimized

--- lib/pages/producer/alerts_page.dart
+++ lib/pages/producer/alerts_page.dart
@@ -6,6 +6,7 @@ import 'package:guardian/models/extensions/string_extension.dart';
 import 'package:guardian/models/providers/read_json.dart';
 
 import 'package:guardian/models/user_alert_notification.dart';
+import 'package:guardian/widgets/custom_circular_progress_indicator.dart';
 import 'package:guardian/widgets/pages/producer/alerts_page/alert_item.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -19,33 +20,29 @@ class AlertsPage extends StatefulWidget {
 }
 
 class _AlertsPageState extends State<AlertsPage> {
+  late Future _future;
+
   List<UserAlertNotification> alerts = [];
-  late String uid;
-  bool isLoading = true;
 
   @override
   void initState() {
-    // getUserDevices().then((devices) {
-    //   getUserAlerts().then((alerts) {
-    //     createAlertNotification(AlertNotification(
-    //       deviceId: devices.first.deviceId,
-    //       alertId: alerts.first.alertId,
-    //     ));
-    //   });
-    // });
-
-    _loadAlerts().then((_) {
-      setState(() => isLoading = false);
-    });
+    _future = _setup();
     super.initState();
+  }
+
+  Future<void> _setup() async {
+    await _loadAlerts();
   }
 
   Future<void> _loadAlerts() async {
     getUserNotifications().then(
       (allAlerts) {
-        alerts = [];
-        alerts.addAll(allAlerts);
-        setState(() => isLoading = false);
+        if (mounted) {
+          setState(() {
+            alerts = [];
+            alerts.addAll(allAlerts);
+          });
+        }
       },
     );
   }
@@ -56,92 +53,93 @@ class _AlertsPageState extends State<AlertsPage> {
     AppLocalizations localizations = AppLocalizations.of(context)!;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          localizations.alerts.capitalize(),
-          style: theme.textTheme.headlineSmall!.copyWith(fontWeight: FontWeight.w500),
+        appBar: AppBar(
+          title: Text(
+            localizations.alerts.capitalize(),
+            style: theme.textTheme.headlineSmall!.copyWith(fontWeight: FontWeight.w500),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    removeAllNotifications().then((_) => loadAlerts());
-                  },
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+        floatingActionButton: FloatingActionButton.extended(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+          backgroundColor: theme.colorScheme.secondary,
+          onPressed: () {
+            Navigator.of(context).pushNamed(
+              '/producer/alert/management',
+              arguments: false,
+            );
+          },
+          label: Text(
+            '${localizations.manage.capitalize()} ${localizations.warnings.capitalize()}',
+            style: theme.textTheme.bodyLarge!.copyWith(
+              color: theme.colorScheme.onSecondary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          icon: Icon(
+            Icons.edit,
+            color: theme.colorScheme.onSecondary,
+          ),
+        ),
+        body: FutureBuilder(
+            future: _future,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CustomCircularProgressIndicator();
+              } else {
+                return SafeArea(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Icon(
-                        Icons.delete_forever,
-                        color: theme.colorScheme.error,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              removeAllNotifications().then((_) => loadAlerts());
+                            },
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.delete_forever,
+                                  color: theme.colorScheme.error,
+                                ),
+                                Text(
+                                  '${localizations.remove.capitalize()} ${localizations.all.capitalize()}',
+                                  style: theme.textTheme.bodyLarge!.copyWith(
+                                    color: theme.colorScheme.error,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        '${localizations.remove.capitalize()} ${localizations.all.capitalize()}',
-                        style: theme.textTheme.bodyLarge!.copyWith(
-                          color: theme.colorScheme.error,
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: ListView.builder(
+                            itemCount: alerts.length,
+                            itemBuilder: (context, index) => AlertItem(
+                              alertNotification: alerts[index],
+                              onRemove: () async {
+                                await removeNotification(
+                                  alerts[index].notificationId,
+                                ).then(
+                                  (_) async => await _loadAlerts(),
+                                );
+                              },
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-            isLoading
-                ? Center(
-                    child: CircularProgressIndicator(
-                      color: theme.colorScheme.secondary,
-                    ),
-                  )
-                : Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: ListView.builder(
-                        itemCount: alerts.length,
-                        itemBuilder: (context, index) => AlertItem(
-                          alertNotification: alerts[index],
-                          onRemove: () async {
-                            await removeNotification(
-                              alerts[index].notificationId,
-                            ).then(
-                              (_) async => await _loadAlerts(),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(100),
-        ),
-        backgroundColor: theme.colorScheme.secondary,
-        onPressed: () {
-          Navigator.of(context).pushNamed(
-            '/producer/alert/management',
-            arguments: false,
-          );
-        },
-        label: Text(
-          '${localizations.manage.capitalize()} ${localizations.warnings.capitalize()}',
-          style: theme.textTheme.bodyLarge!.copyWith(
-            color: theme.colorScheme.onSecondary,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        icon: Icon(
-          Icons.edit,
-          color: theme.colorScheme.onSecondary,
-        ),
-      ),
-    );
+                );
+              }
+            }));
   }
 }
