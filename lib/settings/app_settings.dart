3d78e1c8f7cb278484a0542f25853aed747ee072
Roberto Barreto <robertofmbarreto@gmail.com>
2023-12-20 14:30:22 +0000

    internal network release

--- lib/settings/app_settings.dart
+++ lib/settings/app_settings.dart
@@ -1,9 +1,8 @@
 import 'package:dart_amqp/dart_amqp.dart';
 
-const kGDapiServerUrl =
-    '77.54.1.149:7986'; //'77.54.1.149:7986'; //'0d82-185-51-94-175.ngrok-free.app'; //'192.168.10.71:7986';
-//'3834-185-51-94-175.ngrok-free.app'; // //
+const kGDapiServerUrl = '10.10.128.118:37986'; //'77.54.1.149:7986';
 final kRabbitMqSettings = ConnectionSettings(
-  host: '77.54.1.149',
+  host: '10.10.128.118', //'77.54.1.149',
+  port: 35672,
   authProvider: const PlainAuthenticator("admin", "admin"),
 );
